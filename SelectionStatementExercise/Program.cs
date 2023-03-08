2a21a6a1556b1134c7fc9d906cbb3b0d101c90b1
Nolan Martin <nmartinsdtc@gmail.com>
2023-03-08 11:05:41 -0700

    exercise 1code

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            Console.WriteLine("Enter your favorite number: ");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (favNumber < userInput)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (favNumber > userInput)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You've guessed my favorite number.");
+            }
         }
     }
 }
